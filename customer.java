5ca2c72907f48687cb6e2a06adc08403be7a1c31
jazlynlatif <93916383+jazlynlatif@users.noreply.github.com>
2023-01-10 12:25:10 +0700

    Update and rename src/user.java to customer.java

---
+++ customer.java
@@ -0,0 +1,14 @@
+public class customer{
+    private String customer;
+    private String password;
+    
+    customer(String customerr,String pass){
+        this.customer = customerr;
+        this.password = pass;
+    }
+    
+    void set_data(String customerr,String pass){
+        this.customer = customerr;
+        this.password = pass;
+    }
+}
