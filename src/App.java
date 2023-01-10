4be5bff085c286d4f1466eb9e95c6faaab6ce41c
jazlynlatif <93916383+jazlynlatif@users.noreply.github.com>
2023-01-10 13:25:20 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -18,7 +18,7 @@ public class App
 		System.out.println("Main Page");
 		System.out.println("1. Login");
 		System.out.println("2. Register");
-		System.out.println("Input Choice:");
+		System.out.println("Input Choice (1/2) :");
 		choice = myObj.nextInt();
 		do{
 		if(choice == 1){
