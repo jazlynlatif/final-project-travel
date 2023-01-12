834b9e4d0f17f6f8dc1dcae50a6c3c4f8bcd7081
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-01-12 12:18:50 +0700

    remove unused spaces

--- src/Customer.java
+++ src/Customer.java
@@ -13,12 +13,12 @@ public class Customer extends Account {
     }
     
     void usersetting(){
-        
+	    
     }
     
     void register(){
-		Scanner myObj = new Scanner(System.in);
-		System.out.println("Username: ");
+	    Scanner myObj = new Scanner(System.in);
+	    System.out.println("Username: ");
 	    uname = myObj.next();
 	    System.out.println("Password: ");
 	    upass = myObj.next();
@@ -29,29 +29,30 @@ public class Customer extends Account {
 	    System.out.println("Email: ");
 	    mail = myObj.next();
 	    customer data = new customer(uname, upass, ufname, adrs, mail);
-		System.out.println("Your data has been registered!");
+	    System.out.println("Your data has been registered!");
     }
     
     void login(){
-        int choice;
-		Scanner myObj = new Scanner(System.in);
-		paket travel = new paket(String pakett,String jenisken,String dest,int harga,int jumlah);
-		System.out.println("========================");
-		System.out.println("1. View Profile");
-		System.out.println("2. Modify Profile");
-		System.out.println("3. Choose Travel Package");
-		System.out.println("4. Exit")
-		System.out.println("========================");
-		System.out.println("Input Choice (1-4) :");
-		choice = myObj.nextInt();
-		switch (choice){
+	    int choice;
+	    Scanner myObj = new Scanner(System.in);
+	    paket travel = new paket(String pakett,String jenisken,String dest,int harga,int jumlah);
+	    System.out.println("========================");
+	    System.out.println("1. View Profile");
+	    System.out.println("2. Modify Profile");
+	    System.out.println("3. Choose Travel Package");
+	    System.out.println("4. Exit");
+	    System.out.println("========================");
+	    System.out.println("Input Choice (1-4) :");
+	    choice = myObj.nextInt();
+	    switch (choice){
 		    case 1:
 		        print_data();
 		    case 2:
 		        modify_data();
 		    case 3:
-		        
-		}
+			    
+		    case 4:
+	    }
     }
     
     void set_data(String id, String pass, String cust_name, String address, String cust_email){
