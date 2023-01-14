3d4cbb55d528409b57a47be9ed2219af1d0b8638
jazlynlatif <93916383+jazlynlatif@users.noreply.github.com>
2023-01-14 12:41:00 +0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -12,48 +12,6 @@ public class Customer extends Account {
         this.cust_email.add(cmail);
     }
     
-    // void usersetting(){
-        
-    // }
-    
-//     void register(){
-// 		Scanner myObj = new Scanner(System.in);
-// 		System.out.println("Username: ");
-// 	    uname = myObj.next();
-// 	    System.out.println("Password: ");
-// 	    upass = myObj.next();
-// 	    System.out.println("Full Name: ");
-// 	    ufname = myObj.next();
-// 	    System.out.println("Address: ");
-// 	    adrs = myObj.next();
-// 	    System.out.println("Email: ");
-// 	    mail = myObj.next();
-// 	    customer data = new customer(uname, upass, ufname, adrs, mail);
-// 		System.out.println("Your data has been registered!");
-//     }
-    
-//     void login(){
-//         int choice;
-// 		Scanner myObj = new Scanner(System.in);
-// 		paket travel = new paket(String pakett,String jenisken,String dest,int harga,int jumlah);
-// 		System.out.println("========================");
-// 		System.out.println("1. View Profile");
-// 		System.out.println("2. Modify Profile");
-// 		System.out.println("3. Choose Travel Package");
-// 		System.out.println("4. Exit")
-// 		System.out.println("========================");
-// 		System.out.println("Input Choice (1-4) :");
-// 		choice = myObj.nextInt();
-// 		switch (choice){
-// 		    case 1:
-// 		        print_data();
-// 		    case 2:
-// 		        modify_data();
-// 		    case 3:
-            
-// 		}
-//     }
-    
     void set_data(String id, String pass, String cname, String addr, String cmail){
         super.username.add(id);
         super.password.add(pass);
@@ -62,8 +20,12 @@ public class Customer extends Account {
         this.cust_email.add(cmail);
     }
     
-    void get_data(){
-        return this.username, this.password, this.cust_name, this.address, this.cust_email;
+    void get_data(int i){
+        System.out.println("Username = " + super.username.get(i));
+        System.out.println("Password = " + super.password.get(i));
+        System.out.println("Name = " + this.cust_name.get(i));
+        System.out.println("Address = " + this.address.get(i));
+        System.out.println("Email = " + this.cust_email.get(i));
     }
     
     void modify_data(){
@@ -71,10 +33,12 @@ public class Customer extends Account {
     }
     
     void print_data(){
-        System.out.println("Username = " + super.username);
-        System.out.println("Password = " + super.password)
-        System.out.println("Name = " + this.cust_name);
-        System.out.println("Address = " + this.address);
-        System.out.println("Email = " + this.cust_email);
+        for(int i=0;i<cust_name.size();i++){
+        System.out.println("Username = " + super.username.get(i));
+        System.out.println("Password = " + super.password.get(i));
+        System.out.println("Name = " + this.cust_name.get(i));
+        System.out.println("Address = " + this.address.get(i));
+        System.out.println("Email = " + this.cust_email.get(i));
+        }
     }
 }
