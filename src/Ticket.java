cbdace1092fec2a148f4e132c4c579c1e4b51564
jazlynlatif <93916383+jazlynlatif@users.noreply.github.com>
2023-01-15 15:04:20 +0700

    Create Ticket.java

---
+++ src/Ticket.java
@@ -0,0 +1,15 @@
+// import java.util.*;
+
+// class Ticket extends Customer{
+//     protected ArrayList<String> ticketid = new ArrayList<String>();
+    
+//     Ticket(String id1,String id2,String id3){
+//         super(id1,id2,id3);
+//     }
+    
+//     void set_ticket(){
+//         // this.ticketid.add(Integer.parseInt())
+//     }
+// }
+
+// //Metode Generic
