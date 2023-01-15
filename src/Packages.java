72240452ca1f839f2e6f93a80089f4d644cb8a95
jazlynlatif <93916383+jazlynlatif@users.noreply.github.com>
2023-01-15 15:02:48 +0700

    Update Packages.java

--- src/Packages.java
+++ src/Packages.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
-public class Packages{
+class Packages{
     protected ArrayList<String> namapaket = new ArrayList<String>();
     protected ArrayList<String> jeniskendaraan = new ArrayList<String>();
     protected ArrayList<String> destinasi = new ArrayList<String>();
     protected ArrayList<Integer> price = new ArrayList<Integer>();
     protected ArrayList<Integer> jumlahtiket = new ArrayList<Integer>();
     
-    Packages(String pakett, String jenisken, String dest, int harga, int jumlah){
+    Packages(String pakett,String jenisken,String dest,int harga,int jumlah){
         this.namapaket.add(pakett);
         this.jeniskendaraan.add(jenisken);
         this.destinasi.add(dest);
